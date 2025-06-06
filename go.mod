3a5a6073e67093964ff3da77adb2ffd965245499
Joshua Adebayo <jadebayo@clafiya.com>
2025-06-06 19:54:30 +0100

    Added Squish and String functions to string package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ajosav/GoString
+
+go 1.24.3
--- str.go
+++ str.go
@@ -1,3 +1,21 @@
 package str
 
 import "strings"
+
+func Squish(string string) string {
+	return strings.Join(strings.Fields(string), "")
+}
+
+func Mask(s string, maskChar rune, revealCount int) string {
+	if revealCount <= 0 {
+		return strings.Repeat(string(maskChar), len(s))
+	}
+
+	if revealCount >= len(s) {
+		return s
+	}
+
+	revealedPart := s[:revealCount]
+	maskedLength := len(s) - revealCount
+	return revealedPart + strings.Repeat(string(maskChar), maskedLength)
+}
